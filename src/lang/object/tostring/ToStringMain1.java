2c4c6d674c7d612f78a42e7272031d5755c45710
seokjun01 <suckjungod@naver.com>
2024-09-23 22:36:47 +0900

    tostring()

---
+++ src/lang/object/tostring/ToStringMain1.java
@@ -0,0 +1,12 @@
+package lang.object.tostring;
+
+public class ToStringMain1 {
+    public static void main(String[] args) {
+        Object o = new Object();
+        String s = o.toString();
+
+        System.out.println(s);
+
+        System.out.println(o);
+    }
+}
