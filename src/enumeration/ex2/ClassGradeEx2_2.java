ed464f23d929985df577c6e1d4ce3e608920e66c
seokjun01 <suckjungod@naver.com>
2024-11-13 00:56:51 +0900

    Enum 타입에 대하여 ..

---
+++ src/enumeration/ex2/ClassGradeEx2_2.java
@@ -0,0 +1,15 @@
+package enumeration.ex2;
+
+public class ClassGradeEx2_2 {
+
+    public static void main(String[] args) {
+        int price = 10000;
+
+        DiscountService discountService = new DiscountService();
+        // 외부에서 생성이 가능 ㅡㅡ
+      /*  ClassGrade newClassGrade = new ClassGrade();
+        int result = discountService.discount(newClassGrade, price);
+        System.out.println("newClassGrade의 할인 금액" + result);
+   */
+    }
+}
