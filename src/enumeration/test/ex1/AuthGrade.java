3a44b55cb3aea78fea56092a06eebc343eaa9bd1
seokjun01 <suckjungod@naver.com>
2024-11-14 01:36:19 +0900

    문제와 풀이

---
+++ src/enumeration/test/ex1/AuthGrade.java
@@ -0,0 +1,29 @@
+package enumeration.test.ex1;
+
+public enum AuthGrade {
+    GUEST(1, "손님"), LOGIN(2, "로그인 회원"), ADMIN(3,"관리자");
+
+    private int level;
+    private String description;
+
+    AuthGrade(int level, String description) {
+        this.level = level;
+        this.description = description;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "AuthGrade{" +
+                "level=" + level +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
