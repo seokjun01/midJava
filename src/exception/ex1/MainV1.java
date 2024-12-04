654bec647676c89b9b5bb96da084fb3139941dea
seokjun01 <suckjungod@naver.com>
2024-12-04 17:56:08 +0900

    예외처리가 필요한 상황3

--- src/exception/ex1/MainV1.java
+++ src/exception/ex1/MainV1.java
@@ -6,7 +6,7 @@ public class MainV1 {
 
     public static void main(String[] args) {
 
-        NetworkServiceV1_1 networkService = new NetworkServiceV1_1();
+        NetworkServiceV1_3 networkService = new NetworkServiceV1_3();
 
         Scanner scanner = new Scanner(System.in);
         while(true) {
