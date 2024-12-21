4560703ba4eb76461c11ed7ebd8d10a7fd255712
seokjun01 <suckjungod@naver.com>
2024-12-22 01:22:14 +0900

    정적 중첩 클래스는 new 바깥클래스.중첩클래스() 로 생성할 수 있다.

--- src/nested/nested/NestedOuterMain.java
+++ src/nested/nested/NestedOuterMain.java
@@ -4,7 +4,9 @@ public class NestedOuterMain {
 
     public static void main(String[] args) {
         NestedOuter outer = new NestedOuter();
+        //정적 중첩 클래스는 `new 바깥클래스.중첩클래스()` 로 생성할 수 있다.
         NestedOuter.Nested nested = new NestedOuter.Nested();
         nested.print();
+        System.out.println("nestedClass = " + nested.getClass());
     }
 }
