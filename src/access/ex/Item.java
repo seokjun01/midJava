be4a43a01681b4c62d920fc29c985b48469214fd
seokjun01 <suckjungod@naver.com>
2025-03-03 21:24:29 +0900

    접근제어자 Java복습

--- src/access/ex/Item.java
+++ src/access/ex/Item.java
@@ -1,4 +1,24 @@
 package access.ex;
 
 public class Item {
+
+	private String name;
+	private int price;
+	private int count;
+
+	public Item(String name, int price, int count) {
+		this.name = name;
+		this.price = price;
+		this.count = count;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getTotalPrice() {
+		return price * count;
+	}
+
+
 }
--- src/access/ex/MaxCounter.java
+++ src/access/ex/MaxCounter.java
@@ -1,4 +1,23 @@
 package access.ex;
 
 public class MaxCounter {
+	private int max;
+	private int count;
+
+	public MaxCounter(int max) {
+		this.max = max;
+	}
+
+	public void increment() {
+		this.count ++;
+		if (count >= max) {
+			System.out.println( "최대값을 초과할 수 없습니다.");
+		}
+		return;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
 }
