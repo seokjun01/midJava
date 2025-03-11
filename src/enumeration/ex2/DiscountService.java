1eee32c825e712441ab84fb10fff12b8a32a4927
seokjun01 <suckjungod@naver.com>
2025-03-11 17:39:34 +0900

    [refactor]  ENUM 열거형 리팩토링

--- src/enumeration/ex2/DiscountService.java
+++ src/enumeration/ex2/DiscountService.java
@@ -3,17 +3,6 @@ package enumeration.ex2;
 public class DiscountService {
 
     public int discount(ClassGrade classGrade, int price) {
-        int discountPercent = 0;
-
-        if (classGrade == ClassGrade.BASIC ) {
-            discountPercent = 10;
-        } else if (classGrade == ClassGrade.GOLD) {
-            discountPercent = 20;
-        } else if (classGrade == ClassGrade.DIAMOND) {
-            discountPercent = 30;
-        } else {
-            System.out.println(" 할인 대상이 아닙니다. ");
-        }
-        return price * discountPercent / 100;
+      return price * classGrade.getDiscountPercent() / 100;
     }
 }
