90b1125a3f471001043caa01f81076b6e6c6642f
seokjun01 <suckjungod@naver.com>
2025-03-13 20:03:05 +0900

    익명클래스 사용2

---
+++ src/nested/nested/anonymous/ex/Ex1RefMainV2.java
@@ -0,0 +1,44 @@
+package nested.nested.anonymous.ex;
+
+import java.util.Random;
+
+public class Ex1RefMainV2 {
+
+	public static void hello(Process process) {
+		System.out.println("프로그램 시작");
+
+		process.run();
+
+		System.out.println("프로그램 종료");
+	}
+
+	public static void main(String[] args) {
+
+		class Dice implements Process {
+
+			@Override
+			public void run() {
+				int randomValue = new Random().nextInt(6) + 1;
+				System.out.println("주사위 = " + randomValue);
+			}
+		}
+
+		class Sum implements Process {
+
+			@Override
+			public void run() {
+				for (int i = 1; i <= 3; i++) {
+					System.out.println("i =" + i);
+				}
+			}
+		}
+
+		Process dice = new Dice();
+		Process sum = new Sum();
+
+		System.out.println("Hello 실행");
+		hello(dice);
+		hello(sum);
+
+	}
+}
