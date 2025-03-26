b17bbc33af4420cdf9bb2dbccaccc53ca7ecaaa3
seokjun01 <suckjungod@naver.com>
2025-03-26 18:29:50 +0900

    Map 3

---
+++ src/collection/map/MapMain3.java
@@ -0,0 +1,26 @@
+package collection.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapMain3 {
+
+	public static void main(String[] args) {
+
+		Map<String, Integer> studentMap = new HashMap<>();
+
+		studentMap.put("studentA", 50);
+		System.out.println(studentMap);
+		// 학생이 없는 경우에만 추가1
+		if(!studentMap.containsKey("studentA")) {
+			studentMap.put("studentA", 100);
+		}
+		System.out.println(studentMap);
+
+		//메소드로 존재한다 putIfAbsent
+		studentMap.putIfAbsent("studentA", 100);
+		studentMap.putIfAbsent("studentB", 100);
+		System.out.println(studentMap);
+
+	}
+}
